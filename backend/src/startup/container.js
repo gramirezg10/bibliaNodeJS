c54fc7d435455249ec10705d5d0b4898643b22ad
German Ramirez Gaviria <gramirezg10@gmail.com>
2020-12-12 21:12:19 -0500

    [ADD] controllers added

--- backend/src/startup/container.js
+++ backend/src/startup/container.js
@@ -8,7 +8,7 @@ const app = require('.')
 const { HomeService, UserService, CommentService, IdeaService } = require('../services');
 
 // controllers
-const { HomeController } = require('../controllers');
+const { HomeController, UserController, IdeaController, CommentController } = require('../controllers');
 
 // routes
 const { HomeRoutes } = require('../routes/index.routes');
@@ -35,7 +35,10 @@ container
         CommentService: asClass(CommentService).singleton(),
     })
     .register({
-        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+        HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+        UserController: asClass(UserController.bind(UserController)).singleton(),
+        IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
+        CommentController: asClass(CommentController.bind(CommentController)).singleton(),
     })
     .register({
         HomeRoutes: asFunction(HomeRoutes).singleton()
